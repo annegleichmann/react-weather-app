3fcb802eeacfef89f0ce86fd99ff5b0736a16c3a
annegleichmann <anne.gleichmann@web.de>
2025-01-04 17:35:36 +0100

    added footer with links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/dashboard" target="_blank">
+          Anne Gleichmann
+        </a>
+        , open sourced on{" "}
+        <a
+          href="https://github.com/annegleichmann/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
