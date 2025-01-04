8891812932daac470b173c63f7060cc3dd33bbd8
annegleichmann <anne.gleichmann@web.de>
2025-01-04 22:28:20 +0100

    created basic html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div>
-        Weather App
+        <h1>Weather App</h1>
+        <Weather />
+
         <footer>
           This project was coded by{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input type="search" placeholder="Enter a city..." />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Helsinki</h1>
+      <ul>
+        <li>Wednesday 08:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_couldy.png"
+            alt="Mostly Cloudy"
+          />
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitaion: 12%</li>
+            <li>Humidity: 12%</li>
+            <li>Wind: 3 km/h</li>
+          </ul>
+        </div>
+      </div>
+      Hello from Weather
+    </div>
+  );
+}
