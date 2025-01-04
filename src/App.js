b2cf438e497ae8070bc27cf9bdb70c00b50b541a
annegleichmann <anne.gleichmann@web.de>
2025-01-04 22:10:48 +0100

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/dashboard" rel="noreferrer" target="_blank">
-          Anne Gleichmann
-        </a>
-        , open sourced on{" "}
-        <a
-          href="https://github.com/annegleichmann/react-weather-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div>
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/dashboard"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Anne Gleichmann
+          </a>
+          , open sourced on{" "}
+          <a
+            href="https://github.com/annegleichmann/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
