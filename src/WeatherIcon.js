d9b24deb02f9f98d1bb8bfb73a4787ee500226f3
annegleichmann <anne.gleichmann@web.de>
2025-01-11 10:06:15 +0100

    added basic structure for the forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,22 +3,22 @@ import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "clear-sky-day": "CLEAR_DAY",
-    "clear-sky-night": "CLEAR_NIGHT",
-    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
-    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
-    "broken-clouds-day": "CLOUDY",
-    "broken-clouds-night": "CLOUDY",
-    "shower-rain-day": "RAIN",
-    "shower-rain-night": "RAIN",
-    "rain-day": "RAIN",
-    "rain-night": "RAIN",
-    "thunderstorm-day": "RAIN",
-    "thunderstorm-night": "RAIN",
-    "snow-day": "SNOW",
-    "snow-night": "SNOW",
-    "mist-day": "FOG",
-    "mist-night": "FOG",
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "CLOUDY_DAY",
+    "03n": "CLOUDY_NIGHT",
+    "04d": "SHOWER_RAIN_DAY",
+    "04n": "SHOWER_RAIN_NIGHT",
+    "05d": "RAIN_DAY",
+    "05n": "RAIN_NIGHT",
+    "06d": "THUNDERSTORM_DAY",
+    "06n": "THUNDERSTORM_NIGHT",
+    "07d": "SNOW_DAY",
+    "07n": "SNOW_NIGHT",
+    "08d": "FOG_DAY",
+    "08n": "FOG_NIGHT",
   };
 
   return (
